9ecdc7412f21f1330ff1026c2bb4497ea3526476
HeyzzWatcharin <blackf.c@hotmail.co.th>
2019-11-15 23:42:42 +0700

    edit module bcrypt

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var Database = require("../config/database");
-var bcrypt = require("bcrypt");
+var bcrypt = require("bcryptjs");
 
 
 
