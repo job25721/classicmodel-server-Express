7a753bf26d154057987fe069a746fc0ca49f1ca2
wongwhale <wongwhale_@hotmail.com>
2019-11-20 12:46:11 +0700

    change query

--- routes/admin/product.js
+++ routes/admin/product.js
@@ -1,8 +1,36 @@
 const router = require('express').Router();
 const Database = require("../../config/database");
 
-router.get('/instockItem',function(req,res,next){
-    Database.query('SELECT * FROM products join productlines using (productLine)', function (err, productData, fields) {
+router.get('/instockItem', function(req, res, next) {
+    Database.query('SELECT * FROM products join productlines using (productLine)', function(err, productData, fields) {
+        res.json(productData);
+    })
+})
+
+router.get('/fetchInstockitem/:id', function(req, res, next) {
+    // console.log(req.params.id);
+
+    Database.query(`select * from products as p ,productlines as pl where productCode = '${req.params.id}' and p.productLine = pl.productLine`, function(err, data) {
+        // console.log(data);
+        res.json(data)
+    })
+})
+
+router.get('/changepage/:init', function(req, res, next) {
+    console.log(req.params.init)
+    Database.query(`select * from products as p , productlines as pl where p.productLine = pl.productLine limit ${req.params.init},15`, function(err, data) {
+        res.json(data)
+    })
+})
+
+router.get('/instockItem', function(req, res, next) {
+    Database.query('SELECT * FROM products join productlines using (productLine)', function(err, productData, fields) {
+        res.json(productData);
+    })
+})
+
+router.get('/count', function(req, res, next) {
+    Database.query('SELECT count(*) as count  FROM products join productlines using (productLine)', function(err, productData, fields) {
         res.json(productData);
     })
 })
--- routes/catalog.js
+++ routes/catalog.js
@@ -1,61 +1,82 @@
 const router = require('express').Router();
 const Database = require("../config/database");
 
-router.get('/scaleFilter',function (req,res,next) {
-    Database.query('SELECT DISTINCT productScale FROM products', function (err, data, fields) {
+router.get('/scaleFilter', function(req, res, next) {
+    Database.query('SELECT DISTINCT productScale FROM products', function(err, data, fields) {
         res.json(data);
     });
 })
 
-router.get('/vendorFilter',function (req,res,next) {
-    Database.query('SELECT DISTINCT productVendor FROM products', function (err, data, fields) {
+router.get('/vendorFilter', function(req, res, next) {
+    Database.query('SELECT DISTINCT productVendor FROM products', function(err, data, fields) {
         res.json(data);
     });
 })
 
-router.get('/fetchPopUp/:productCode',function (req,res,next) {
+router.get('/fetchPopUp/:productCode', function(req, res, next) {
     var code = req.params.productCode
-    Database.query('SELECT * FROM `products` JOIN `productlines` USING (productLine) where productCode = ? ', [code], function (err, result, fields) {
-        res.json( result)
+    Database.query('SELECT * FROM `products` JOIN `productlines` USING (productLine) where productCode = ? ', [code], function(err, result, fields) {
+        res.json(result)
     })
 })
-router.get('/getData',function (req,res,next) {
-        var scale = req.query.scale;
-        var vendor = req.query.vendor; 
-        console.log(scale);
-        
-        if ((scale === undefined && vendor === undefined) || (scale === "All" && vendor === "All")) {
-            Database.query('SELECT * FROM `products` JOIN `productlines` USING (productLine)', function (err, result, fields) {
 
-                res.json({
-                    result: result,
-                    rowNum: result.length
-                })
-            });
-        } else if (scale === "All" && vendor !== "All") {
-            Database.query('SELECT * FROM `products` JOIN `productlines` USING (productLine) where productVendor = ?', vendor, function (err, result, fields) {
+router.get('/allproduct', function(req, res, next) {
 
-                res.json({
-                    result: result,
-                    rowNum: result.length
-                })
-            })
-        } else if (scale !== "All" && vendor === "All") {
-            Database.query('SELECT * FROM `products` JOIN `productlines` USING (productLine) where productScale = ?', scale, function (err, result, fields) {
-
-                res.json({
-                    result: result,
-                    rowNum: result.length
-                })
-            })
-        } else {
-            Database.query('SELECT * FROM `products` JOIN `productlines` USING (productLine) where productScale = ? and productVendor = ?', [scale, vendor], function (err, result, fields) {
-
-                res.json({
-                    result: result,
-                    rowNum: result.length
-                })
-            })
-        }
+    Database.query(`select * from products join productlines USING (productLine)`, function(err, result, fields) {
+        res.json({
+            result: result,
+            row: result.length
+        })
+    })
 })
-module.exports =  router ;
+
+router.get('/test/:scale/:vendor/:name/:init', function(req, res, next) {
+    var scale = req.params.scale
+    var vendor = req.params.vendor
+    var name = req.params.name
+    var init = req.params.init
+    var filter = ""
+    if (name == "undefined") {
+        if (scale == "All" && vendor == "All") filter = ""
+        else if (scale == "All" && vendor != "All") filter = 'where productVendor = "' + vendor + '"'
+        else if (scale != "All" && vendor == "All") filter = 'where productScale = "' + scale + '"'
+        else filter = 'where productScale = "' + scale + '" and productVendor="' + vendor + '"'
+    } else {
+        if (scale == "All" && vendor == "All") filter = 'where productName="' + name + '"'
+        else if (scale == "All" && vendor != "All") filter = 'where productVendor = "' + vendor + '"and productName="' + name + '"'
+        else if (scale != "All" && vendor == "All") filter = 'where productScale = "' + scale + '"and productName="' + name + '"'
+        else filter = 'where productScale = "' + scale + '" and productVendor="' + vendor + '"and productName="' + name + '"'
+    }
+    Database.query(`select * from products join productlines USING (productLine) ${filter} limit ${init},18`, function(err, result, fields) {
+        res.json({
+            result: result,
+            row: result.length
+        })
+    })
+})
+
+router.get('/product/:scale/:vendor/:name', function(req, res, next) {
+    var scale = req.params.scale
+    var vendor = req.params.vendor
+    var name = req.params.name
+    var filter = ""
+    if (name == "undefined") {
+        if (scale == "All" && vendor == "All") filter = ""
+        else if (scale == "All" && vendor != "All") filter = 'where productVendor = "' + vendor + '"'
+        else if (scale != "All" && vendor == "All") filter = 'where productScale = "' + scale + '"'
+        else filter = 'where productScale = "' + scale + '" and productVendor="' + vendor + '"'
+    } else {
+        if (scale == "All" && vendor == "All") filter = 'where productName="' + name + '"'
+        else if (scale == "All" && vendor != "All") filter = 'where productVendor = "' + vendor + '"and productName="' + name + '"'
+        else if (scale != "All" && vendor == "All") filter = 'where productScale = "' + scale + '"and productName="' + name + '"'
+        else filter = 'where productScale = "' + scale + '" and productVendor="' + vendor + '"and productName="' + name + '"'
+    }
+    Database.query(`select * from products join productlines USING (productLine) ${filter} `, function(err, result, fields) {
+        res.json({
+            result: result,
+            row: result.length
+        })
+    })
+})
+
+module.exports = router;
