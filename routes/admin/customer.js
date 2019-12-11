1c22369e1db55a2e032ffdde5be78aae84a5fa54
HeyzzWatcharin <blackf.c@hotmail.co.th>
2019-12-11 19:10:07 +0700

    Update

--- routes/admin/customer.js
+++ routes/admin/customer.js
@@ -2,22 +2,29 @@ const router = require('express').Router();
 const Database = require("../../config/database");
 
 router.get('/fetch', function(req, res, next) {
-    Database.query(`select * from customers`, function(err, data) {
-        console.log(data.length);
-        res.json(data)
+    Database.query(`select * from customers where salesRepEmployeeNumber = ${req.session.user}`, function(err, data) {
+        if(data.length > 0) {
+            console.log(data);
+            console.log(req.session.user);
+            
+            
+            res.json(data)
+        }
+        else res.json({permission : false})
+        
     })
 })
 
 router.get('/changepage/:init', function(req, res, next) {
     var init = req.params.init
-    Database.query(`select * from customers limit ${init},15`, function(err, data) {
+    Database.query(`select * from customers where salesRepEmployeeNumber = ${req.session.user} limit ${init},15`, function(err, data) {
         res.json(data)
     })
 })
 
 router.get('/detail/:customerNumber', function(req, res, next) {
     var customerNumber = req.params.customerNumber
-    Database.query(`select * from customerswhere customerNumber = ${customerNumber}`, function(err, data) {
+    Database.query(`select * from customers where customerNumber = ${customerNumber}`, function(err, data) {
         res.json(data)
     })
 })
